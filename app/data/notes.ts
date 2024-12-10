21b09a230a1dbc31e44613d46fcbfab3337a37ba
himanshu12yadav <himanshuyadav.12jan@gmail.com>
2024-12-10 12:15:37 +0530

    10/12/24-12:14

--- app/data/notes.ts
+++ app/data/notes.ts
@@ -1,11 +1,11 @@
 import fs from 'fs/promises';
 
 export async function getStoredNotes(){
-    const rawFileContent = await fs.readFile('notes.json',{encoding:'utf-8'});
+    const rawFileContent = await fs.readFile('Notes.json',{encoding:'utf-8'});
     const data = JSON.parse(rawFileContent);
     return data.notes ?? [];
 }
-
+//@ts-ignore
 export function storeNotes(notes:any){
-    return fs.writeFile('notes.json',JSON.stringify({notes:notes || []}));
+    return fs.writeFile('Notes.json',JSON.stringify({notes:notes || []}));
 }
